d912ed3d0327119fd555e8c587c80aebbd0c4ca0
saic80744-sketch <saic80744@gmail.com>
2026-02-16 23:18:10 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App
+ * UC1: Print OOPS to console
+ * @author sai
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
