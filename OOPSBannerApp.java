abb2f075e9b4139ee3bc723608acd062993eff1a
saic80744-sketch <saic80744@gmail.com>
2026-02-18 23:33:34 +0530

    UC5: Inline array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,16 @@
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", " ***** ", " ***** "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*     *", "*      "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", " ***** ", " ***** "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*      ", "      *"));
-
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", "*      ", " ***** "));
+    public static void main(String[] args){
+        String[] lines = {
+            String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "),
+            String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
+            String.join(" ", "**     **", "**     **", "**     **", "**       "),
+            String.join(" ", "**     **", "**     **", "**   **  ", "  *****  "),
+            String.join(" ", "**     **", "**     **", "******   ", "      ** "),
+            String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** "),
+            String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
