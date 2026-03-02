fa4ad29109815ee6516fe1448a4ba5fa1f0935fe
saic80744-sketch <saic80744@gnail.com>
2026-03-02 08:52:32 +0530

    UC7: Refactor banner using InnerClass

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,86 @@
 public class OOPSBannerApp {
 
+    
+    public static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        
+        public char getCharacter() {
+            return character;
+        }
+
+        
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    
+    public static String[] getOPattern() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    
+    public static String[] getPPattern() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    
+    public static String[] getSPattern() {
+        return new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        };
+    }
+
+    
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
-        lines[0] = String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** ");
-        lines[1] = String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      ");
-        lines[2] = String.join(" ", "**     **", "**     **", "**     **", "**       ");
-        lines[3] = String.join(" ", "**     **", "**     **", "**   **  ", "  *****  "); 
-        lines[4] = String.join(" ", "**     **", "**     **", "******   ", "      ** ");
-        lines[5] = String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** ");
-        lines[6] = String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ");
-        for(String line : lines){
+        CharacterPatternMap[] letters = {
+                new CharacterPatternMap('O', getOPattern()),
+                new CharacterPatternMap('O', getOPattern()),
+                new CharacterPatternMap('P', getPPattern()),
+                new CharacterPatternMap('S', getSPattern())
+        };
+
+        // Print row by row using StringBuilder
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (CharacterPatternMap letter : letters) {
+                line.append(letter.getPattern()[row]).append("  ");
+            }
+
             System.out.println(line);
         }
     }
